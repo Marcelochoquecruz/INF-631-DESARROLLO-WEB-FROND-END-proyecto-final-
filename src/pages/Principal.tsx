3f3c54973dd1b0b9e3f3fb75b8a808a292714dc4
Marcelo Choque Cruz <chelisimo27@gmail.com>
2024-12-08 04:52:31 -0600

    Portada portafolio funciona+foto

--- src/pages/Principal.tsx
+++ src/pages/Principal.tsx
@@ -2,14 +2,12 @@
 import Menu from '../components/menu';
 import Footer from '../components/footer';
 import Contenidop from './contenidop';
-import Carrousel from '../components/slider';
 
 const Principal = () => {
   return (
     <div>
       {/* Otro contenido específico de la página principal */}
       <Menu />
-      <Carrousel />
       <Contenidop/>
       <Footer/>
     </div>
